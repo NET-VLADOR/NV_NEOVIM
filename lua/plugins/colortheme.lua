b462366f8a5ac8c72a6cc0e32fbe2a5f8da4c903
NET-VLADOR <lolipop120999@gmail.com>
2025-06-12 18:07:46 +0500

    init commit

---
+++ lua/plugins/colortheme.lua
@@ -0,0 +1,43 @@
+return {
+	'catppuccin/nvim',
+	name = 'catppuccin',
+	priority = 1000,
+	config = function()
+		-- Состояние прозрачности (по умолчанию включено)
+		local bg_transparent = true
+
+		-- Базовые настройки темы
+		local catppuccin_config = {
+			flavour = 'mocha', -- latte, frappe, macchiato, mocha
+			transparent_background = bg_transparent,
+			-- Добавьте другие настройки здесь
+		}
+
+		-- Применение конфигурации
+		require('catppuccin').setup(catppuccin_config)
+		vim.cmd.colorscheme 'catppuccin'
+
+		-- Функция переключения прозрачности
+		local function toggle_transparency()
+			bg_transparent = not bg_transparent
+
+			-- Обновляем конфиг
+			catppuccin_config.transparent_background = bg_transparent
+			require('catppuccin').setup(catppuccin_config)
+
+			-- Перезагружаем тему
+			vim.cmd.colorscheme 'catppuccin'
+
+			-- Опционально: уведомление о состоянии
+			local state = bg_transparent and 'ON' or 'OFF'
+			print('Background transparency: ' .. state)
+		end
+
+		-- Назначение горячей клавиши
+		vim.keymap.set('n', '<leader>bg', toggle_transparency, {
+			noremap = true,
+			silent = true,
+			desc = 'Toggle background transparency',
+		})
+	end,
+}
