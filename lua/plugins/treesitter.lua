1e714181165754c1134007c172dd9ec1d7f4f91c
NET-VLADOR <lolipop120999@gmail.com>
2025-06-15 15:13:28 +0500

    feat(treesitter): add incremental selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,50 +1,62 @@
 return {
+  -- https://github.com/nvim-treesitter/nvim-treesitter
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate', -- Команда обновления парсеров
-  main = 'nvim-treesitter.configs', -- Основной модуль конфигурации
-  opts = {
-    -- Список языков для установки
-    ensure_installed = {
-      'lua',
-      'javascript',
-      'typescript',
-      'vimdoc',
-      'vim',
-      'regex',
-      'sql',
-      'dockerfile',
-      'toml',
-      'json',
-      'java',
-      'c_sharp',
-      'gitignore',
-      'yaml',
-      'markdown',
-      'markdown_inline',
-      'bash',
-      'tsx',
-      'css',
-      'html',
-    },
+  config = function()
+    local configs = require 'nvim-treesitter.configs'
 
-    -- Автоустановка отсутствующих парсеров
-    auto_install = true,
+    configs.setup {
+      -- Список языков для установки
+      ensure_installed = {
+        'lua',
+        'javascript',
+        'typescript',
+        'vimdoc',
+        'vim',
+        'regex',
+        'sql',
+        'dockerfile',
+        'toml',
+        'json',
+        'java',
+        'c_sharp',
+        'gitignore',
+        'yaml',
+        'markdown',
+        'markdown_inline',
+        'bash',
+        'tsx',
+        'css',
+        'html',
+      },
 
-    -- Настройки подсветки
-    highlight = {
-      enable = true, -- Включить подсветку синтаксиса
-      -- Языки, требующие regex-подсветки Vim
-      additional_vim_regex_highlighting = { 'javascript' },
-    },
+      -- Автоустановка отсутствующих парсеров
+      auto_install = true,
+      sync_install = false,
 
-    -- Настройки автоотступов
-    indent = {
-      enable = true, -- Включить автоотступы
-    },
-  },
+      -- Настройки подсветки
+      highlight = {
+        enable = true, -- Включить подсветку синтаксиса
+      },
 
-  -- Примечание: Дополнительные модули Treesitter:
-  --   - Инкрементальное выделение (встроено)
-  --   - Показ контекста: https://github.com/nvim-treesitter/nvim-treesitter-context
-  --   - Текстобъекты: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      -- Настройки автоотступов
+      indent = {
+        enable = true, -- Включить автоотступы
+      },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = '<Enter>',
+          node_incremental = '<Enter>',
+          scope_incremental = false,
+          node_decremental = '<Backspace>',
+        },
+      },
+    }
+    -- Примечание: Дополнительные модули Treesitter:
+    --   - Инкрементальное выделение (встроено)
+    --   - Показ контекста: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --   - Текстобъекты: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+  end,
 }
