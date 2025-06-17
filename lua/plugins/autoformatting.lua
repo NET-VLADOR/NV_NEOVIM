a48b58a5493f39388a4c174fda5b42a06c768dcb
NET-VLADOR <lolipop120999@gmail.com>
2025-06-17 22:41:46 +0500

    feat(autoformatting): add jsx and tsx formatting support

--- lua/plugins/autoformatting.lua
+++ lua/plugins/autoformatting.lua
@@ -4,8 +4,10 @@ return {
   opts = {
     formatters_by_ft = {
       lua = { 'stylua' },
-      javascript = { 'prettierd', stop_after_first = true },
-      typescript = { 'prettierd', stop_after_first = true },
+      javascript = { 'prettierd' },
+      javascriptreact = { 'prettierd' },
+      typescript = { 'prettierd' },
+      typescriptreact = { 'prettierd' },
     },
     format_on_save = {
       timeout_ms = 500,
